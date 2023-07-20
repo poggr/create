98c7df81fb6a6f69f2d09f178a562b7eebf07dd4
poggr <117246579+poggr@users.noreply.github.com>
2023-07-20 07:32:22 +0545

    Update connections.php

--- connections.php
+++ connections.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="bijaydatabase";
+$db="bidushidatabase";
 $conn=mysqli_connect($host, $user, $pass, $db);
 if(!$conn){echo "Unable to connect to database";}
 ?>
